d329268c1a85d18c3063569560e202e933689005
Maaviya <maaviya.f@gmail.com>
2025-06-20 16:19:47 +0100

    completed challenge

--- unit_four_more_complex/lib/age_checker.py
+++ unit_four_more_complex/lib/age_checker.py
@@ -5,12 +5,18 @@ from datetime import datetime
 def AgeChecker(dob):
     required_age = 16
     current_date = datetime.today()
-    converted_usr_input = datetime.strptime(dob, "%Y-%m-%d")
-    user_age = 0
-    
-    print(current_date)
-    print(converted_usr_input)
+    try: 
+        converted_usr_input = datetime.strptime(dob, "%Y-%m-%d")
+        age_in_days = str(current_date - converted_usr_input).split(" ")
+        user_age =  (int(int(age_in_days[0])/365.25))
+        if user_age < 16:
+            return f'Access Denied! You are {user_age} you need to be {required_age}'
+        else:
+            return "Access granted!"
+        
+    except ValueError as ve:
+        print("Please enter a datetime in YYYY-MM-DD")
 
-
-    if user_age < 16:
-        return f'Access Denied! You are {user_age} you need to be {required_age}'
+    # print(current_date)
+    # print(converted_usr_input)
+    #print(f"testing age {(current_date - converted_usr_input)}")
--- unit_four_more_complex/tests/test_age_checker.py
+++ unit_four_more_complex/tests/test_age_checker.py
@@ -1,5 +1,8 @@
 import lib.age_checker
 from lib.age_checker import AgeChecker
+import datetime
+from datetime import date
+from datetime import datetime
 
 
 def test_check_AgeChecker_func_exists():
@@ -7,3 +10,12 @@ def test_check_AgeChecker_func_exists():
 
 def test_user_enters_age_too_young():
     assert AgeChecker("2015-06-20") == 'Access Denied! You are 10 you need to be 16'
+
+def test_user_enters_age_old_enough():
+    assert AgeChecker("1990-02-18") == "Access granted!"
+
+def test_user_enter_16():
+    assert AgeChecker("2009-06-20") == "Access granted!"
+
+def test_user_enters_incorrect_dob():
+    AgeChecker("092390") == "Please enter a datetime in YYYY-MM-DD"
